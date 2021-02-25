f5c76b32ed1b87935f3a860209fccd3089678fe0
Markki Meyer <markkimeyer@gmail.com>
2021-02-24 19:22:29 -0600

    able to push to html and customized prompts by role

--- Develop/app2.js
+++ Develop/app2.js
@@ -4,13 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "main.html");
-
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-
-
 // Write code to use inquirer to gather information about the development team members,
 const employees = [];
 const employeeInput = [
@@ -77,6 +73,29 @@ const engineerInput = [
     }
 ];
 
+const internInput = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the first and last name:"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter the employee's ID number:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter the employee's email address:"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Enter the intern's university:"
+    }
+];
+
 // and to create objects for each team member (using the correct classes as blueprints!)
 const addEmployee = () => {
     inquirer.prompt([
@@ -98,56 +117,66 @@ const addEmployee = () => {
     });
 }
 
-
-const addRole = () => {
+function addRole() {
     inquirer.prompt([
         {
             type: "list",
             name: "role",
             message: "Choose the employee's role: ",
-            choices: ["Engineer", "Manager", "Intern"]
+            choices: ["Engineer", "Manager", "Intern"],
         }
-    ]).then(function (roleResponse) {
-        if (roleResponse.role === 'Manager') {
-            inquirer.prompt(managerInput).then((response) => {
-                const newManager = new Manager(response.name, response.id, response.email, response.officenumber);
-                employees.push(newManager);
-                console.log(employees);
-                addEmployee();
-
-    else (response.role === 'Engineer') {
-    const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
-    employees.push(newEngineer);
-    console.log(employees);
+    ]).then((roleResponse) => {
+        if (roleResponse.role == 'Engineer') {
+            console.log(roleResponse);
+            inquirer.prompt(engineerInput).then((response) => 
+            {
+            const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
+            employees.push(newEngineer);
+            addEmployee();
+            })
+        }
+       else if (roleResponse.role == 'Manager') {
+            console.log(roleResponse);
+            inquirer.prompt(managerInput).then((response) => 
+            {
+            const newManager = new Manager(response.name, response.id, response.email, response.officeNumber);
+            employees.push(newManager);
+            addEmployee();
+            })
+        }
+        else if (roleResponse.role == 'Intern') {
+            console.log(roleResponse);
+            inquirer.prompt(internInput).then((response) => 
+            {
+            const newIntern= new Intern(response.name, response.id, response.email, response.school);
+            employees.push(newIntern);
+            addEmployee();
+            })
+        }
+    })
 }
-            
 
-
-
-const createObjects = () => {
-    if (response.role === 'Manager') {
-        inquirer.prompt(employeeInput).then((response) => {
-            console.log(response);
+function init() {
+    return inquirer.prompt(employeeInput).then((response) => {
+        console.log(response);
+        if (response.role === 'Manager') {
             const newManager = new Manager(response.name, response.id, response.email, response.officenumber);
             employees.push(newManager);
-            console.log(employees);
+            console.log(employees)
             addEmployee();
         }
+        else if (response.role === 'Engineer') {
+            const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
+            employees.push(newEngineer);
+            console.log(employees);
+        }
 
-            else if (response.role === 'Engineer') {
-        const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
-        employees.push(newEngineer);
-        console.log(employees);
-    }
-
-    else if (response.role === 'Intern') {
-        const newIntern = new Intern(response.name, response.id, response.email, response.role, response.school);
-        employees.push(newIntern);
-        console.log(employees);
-    }
-                )
-
-    }
+        else if (response.role === 'Intern') {
+            const newIntern = new Intern(response.name, response.id, response.email, response.role, response.school);
+            employees.push(newIntern);
+            console.log(employees);
+        }
+    })
 }
 addEmployee();
 
@@ -155,17 +184,14 @@ addEmployee();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
